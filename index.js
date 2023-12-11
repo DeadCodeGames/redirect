ac917e899f5bd98bc7ca1494d8ff53ea85285d64
Richard Kužela <richard@kanshen.click>
2023-12-11 11:21:57 +0100

    Added a simple redirect base

---
+++ index.js
@@ -0,0 +1,12 @@
+function getURL() {
+    return new URLSearchParams(window.location.search).get("id");
+}
+
+async function redirect(ID) {
+    const list = await fetch("index.json").then(response => response.json());
+    if (list[ID]) {
+        document.querySelector("body").appendChild('<span id="name">' + list[ID]["name"] + '</span>')
+    }
+}
+
+onDOMContentLoaded = () =>{redirect(getURL())}

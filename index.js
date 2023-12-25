2b5a567eea8e731f424c328d0a4da6a2a738e35e
Richard Kužela <richard@kanshen.click>
2023-12-25 12:15:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,21 @@ async function redirect(ID) {
                 window.location.href = list[ID]["redir"];
             }, 1000);
         }, Math.random()*1500);
+    } else {
+        const spanname = document.createElement('span');
+        spanname.id = "name";
+        spanname.textContent = "deadcode";
+        setTimeout(() => {
+            document.querySelector("body").querySelector("span").parentNode.insertBefore(spanname, document.querySelector("body").querySelector("span").nextSibling); setOwO(12);
+            setTimeout(() => {
+                window.location.href = "https://deadcode.is-a.dev/";
+            }, 1000);
+        }, Math.random()*1500);
     }
 }
 
-function setOwO() {
+function setOwO(predefinedOwO) {
     const owo = ["owo", "OwO", "oWo", "Ōwò", "uwu", "UwU", "uWu", "iwi", "qwq", "pwp", "TwT", "тwт", "TmT", "тmт", "ôWô", "òwó", "ùwú", ">w<", "-w-", ">m<", "omo", "OmO", "umu", "UmU", "-m-", "owø", "^w^"];
-    document.querySelector("uwu").textContent = owo[Math.round(Math.random() * owo.length)];
+    document.querySelector("uwu").textContent = owo[predefinedOwO] || owo[Math.round(Math.random() * owo.length)];
 }
 redirect(getURL());
